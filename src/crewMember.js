33ebdee05e2b667aee16bc5e398c2aa54e63b7c8
yendai-vo <yendai.vo@hotmail.com>
2018-08-31 15:52:53 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,29 @@
-class CrewMember {}
+class CrewMember {
+    constructor(position){
+        this.position = position
+        this.currentShip = "Looking for a Rig"
+    }
+
+    engageWarpDrive() {
+        if (this.currentShip === 'Looking for a Rig') {
+            return 'had no effect';
+        }
+    }
+
+    setsInvisibility() {
+        if (this.currentShip === 'Looking for a Rig') {
+            return 'had no effect';
+        }
+        if (this.position === 'Defender') {
+            // debugger
+            this.currentShip.cloaked = true;
+        }
+    }
+
+    chargePhasers() {
+        if (this.currentShip === 'Looking for a Rig') {
+            return 'had no effect';
+        }
+    }
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,24 @@
-class Spaceship {}
+class Spaceship {
+    constructor(name, crew) {
+        this.name = name
+        this.phasers = 5
+        this.shields = 4
+        this.cloaked = false
+        this.warpDrive = 'disengaged'
+        this.docked = true
+        this.phasersCharge = 'uncharged'
+        this.crew = crew 
+            if(this.crew.length) {
+                this.docked = false
+            } else {
+                this.docked = true
+            }
+        this.setCrew()
+    }
+
+    setCrew() {
+        this.crew.forEach(crewMember => (crewMember.currentShip = this))
+    }
+
+
+}
